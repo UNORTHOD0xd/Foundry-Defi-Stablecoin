16d273bcf01341bfae85fc6568841cf90395f792
Dorien Bourne <dorienjoshuabourne@gmail.com>
2025-10-15 00:00:45 -0500

    Decentralized Stablecoin contract setup

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.20;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title Decentralized Stable Coin (DSC)
+ * @author unorthod0xd
+ * @notice This contract is for a stablecoin that is pegged to the USD ($1.00)
+ * @notice This stablecoin is backed by crypto assets (e.g., ETH, BTC)
+ * @notice This stablecoin has a liquidation mechanism to ensure it maintains its peg
+ * @dev This contract is a simplified version and does not include all functionalities
+ */
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    constructor() ERC20("Decentralized Stable Coin", "DSC") Ownable(0xF31A99A843bA137e19b4146c4FEa19B5A6f0c435) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount); // Call the parent contract's burn function
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+}
