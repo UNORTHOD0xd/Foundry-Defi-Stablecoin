677795118215188a8b435a0de29e525536bef1fb
Dorien Bourne <dorienjoshuabourne@gmail.com>
2025-11-10 23:23:32 -0500

    Add deposit collateral test infrastructure and zero-amount validation test
      - Add USER address and collateral constants for testing
      - Mint initial WETH balance to test user in setUp
      - Implement testRevertsIfCollateralZero to verify zero-amount protection
      - Mark testGetUsdValue as view function
    
      The changes set up the testing infrastructure for deposit collateral functionality and add the first test validating that depositing zero collateral properly reverts with the expected error.

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -7,6 +7,7 @@ import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "../../test/mocks/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
@@ -16,17 +17,23 @@ contract DSCEngineTest is Test {
     address ethUsdPriceFeed;
     address weth;
 
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+
     function setUp() public {
         deployer = new DeployDSC();
         (dsc, dsce, config) = deployer.run();
         (ethUsdPriceFeed,, weth,,) = config.activeNetworkConfig();
+
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
     }
 
     ///////////////////////////////
     ///////// PRICE TEST //////////
     ///////////////////////////////
 
-    function testGetUsdValue() public {
+    function testGetUsdValue() public view{
         uint256 ethAmount = 15e18;
         uint256 expectedUsd = 30000e18;
         uint256 actualUsd = dsce.getUsdValue(weth, ethAmount);
@@ -34,4 +41,16 @@ contract DSCEngineTest is Test {
     }
 
 
+    ////////////////////////////////////////
+    /////// DEPOSIT COLLATERAL TESTS////////
+    ////////////////////////////////////////
+
+    function testRevertsIfCollateralZero() public{
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        dsce.depositCollateral(weth, 0);
+        vm.stopPrank();
+    }
 }
