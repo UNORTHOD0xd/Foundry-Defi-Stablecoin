07b77598a94dbeef890d33c30ae4c4c533ae12df
Dorien Bourne <dorienjoshuabourne@gmail.com>
2026-01-06 13:37:20 -0500

    Add fuzz testing setup and invariant test scaffolding
    Learning about fuzz testing and introduction to invariant testing:
    
    - Add invariant configuration to foundry.toml:
      - runs: 128 (number of test runs)
      - depth: 128 (call sequence depth)
      - fail_on_revert: false (continue on reverts)
    
    - Create test/fuzz directory with initial scaffolding:
      - Invariants.T.sol: Document key protocol invariants
        - Total DSC supply < total collateral value
        - Getter view functions should never revert
      - Handlers.T.sol: Placeholder for handler functions
    
    - Update .gitignore to track testing documentation

---
+++ test/fuzz/Invariants.T.sol
@@ -0,0 +1,6 @@
+// Have our Invariants/Properties
+
+// What are our invariants? 
+
+// e.g. The total supply of DSC should be less than the total value of collateral.
+// Getter view functions should never revert <- Evergreen Invariant 
